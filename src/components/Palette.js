cdc1678652108fd8013490e8991e57f5a5742224
Alex <alex.kreinis@walla.co.il>
2019-07-22 02:20:44 +0300

    Added palette colors

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox.js';
+import '../css/Palette.css';
 
 export class Palette extends Component {
+  makingColors = () => {
+    return this.props.colors.map(color => {
+      return <ColorBox background={color.color} name={color.name} />;
+    });
+  };
   render() {
+    const colorBoxes = this.makingColors();
     return (
       <div className="Palette">
-        <div className="Palette-colors">{/* bunch of colors*/}</div>
+        <div className="Palette-colors"> {colorBoxes}</div>
       </div>
     );
   }
