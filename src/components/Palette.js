db94f53fb533173c017fd311830324192c157b24
Alex <alex.kreinis@walla.co.il>
2019-07-22 01:38:10 +0300

    created first palette

---
+++ src/components/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">{/* bunch of colors*/}</div>
+      </div>
+    );
+  }
+}
+
+export default Palette;
